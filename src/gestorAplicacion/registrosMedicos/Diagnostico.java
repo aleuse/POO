1c97775265757abada8da1108c90b4df205a6efd
Luzarait1 <111089098+Luzarait1@users.noreply.github.com>
2022-10-07 08:05:15 -0500

    Clase Diagnostico

--- src/gestorAplicacion/registrosMedicos/Diagnostico.java
+++ src/gestorAplicacion/registrosMedicos/Diagnostico.java
@@ -1,5 +1,50 @@
-package gestorAplicacion.registrosMedicos;
+package registrosMedicos;
+import java.util.ArrayList;
 
 public class Diagnostico {
+    private ArrayList<String> sintomas = new ArrayList<String>();
+    private String observacion;
+    private String medicamiento;
+    private int id;
+   
+    public Diagnostico(ArrayList<String> sintomas, String observacion, String medicamiento, int id) {
+        this.sintomas = sintomas;
+        this.observacion = observacion;
+        this.medicamiento = medicamiento;
+        this.id = id;
+    }
 
+    public ArrayList<String> getSintomas() {
+        return sintomas;
+    }
+
+    public void setSintomas(ArrayList<String> sintomas) {
+        this.sintomas = sintomas;
+    }
+
+    public String getObservacion() {
+        return observacion;
+    }
+
+    public void setObservacion(String observacion) {
+        this.observacion = observacion;
+    }
+
+    public String getMedicamiento() {
+        return medicamiento;
+    }
+
+    public void setMedicamiento(String medicamiento) {
+        this.medicamiento = medicamiento;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    
 }
+
