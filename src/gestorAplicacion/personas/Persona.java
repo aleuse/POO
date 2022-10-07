0f7c24f46fc1d7c70199c2747b9b0a2f9d93da7c
Nat <80775151+Nat411@users.noreply.github.com>
2022-10-06 23:54:15 -0500

    Persona y tipoDocumento

--- src/gestorAplicacion/personas/Persona.java
+++ src/gestorAplicacion/personas/Persona.java
@@ -1,5 +1,91 @@
 package gestorAplicacion.personas;
 
 public class Persona {
-
+	protected String nombre;
+	protected String apellido;
+	protected tipoDocumento documento;
+	protected long numeroDocumento;
+	protected int edad;
+	protected String genero;
+	protected long telefono;
+	protected String correoElectronico;
+	protected String direccion;
+	
+	public Persona(String nombre, String apellido, tipoDocumento documento, long numeroDocumento, int edad, String genero, long telefono, String correoElectronico, String direccion) {
+		setNombre(nombre);
+		setApellido(apellido);
+		setDocumento(documento);
+		setNumeroDocumento(numeroDocumento);
+		setEdad(edad);
+		setGenero(genero);
+		setTelefono(telefono);
+		setCorreoElectronico(correoElectronico);
+		setDireccion(direccion);
+	}
+	public Persona() {
+		this(null,null, null, 0, 0, null, 0, null, null);
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	
+	public tipoDocumento getDocumento() {
+		return documento;
+	}
+	public void setDocumento(tipoDocumento documento) {
+		this.documento = documento;
+	}
+	
+	public long getNumeroDocumento() {
+		return numeroDocumento;
+	}
+	public void setNumeroDocumento(long numeroDocumento) {
+		this.numeroDocumento = numeroDocumento;
+	}
+	
+	public int getEdad() {
+		return edad;
+	}
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	
+	public String getGenero() {
+		return genero;
+	}
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+	
+	public long getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(long telefono) {
+		this.telefono = telefono;
+	}
+	
+	public String getCorreoElectronico() {
+		return correoElectronico;
+	}
+	public void setCorreoElectronico(String correoElectronico) {
+		this.correoElectronico = correoElectronico;
+	}
+	
+	public String getDireccion() {
+		return direccion;
+	}
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
 }
---
+++ src/gestorAplicacion/personas/tipoDocumento.java
@@ -0,0 +1,5 @@
+package gestorAplicacion.personas;
+
+enum tipoDocumento {
+	CEDULA, TARJETA_DE_IDENTIDAD, CEDULA_DE_EXTRANJERIA
+}
