7fb9eb334eae25fcf38ab346df249bb153572789
Nat <80775151+Nat411@users.noreply.github.com>
2022-10-07 00:15:54 -0500

    Paciente parte 1

--- src/gestorAplicacion/personas/Paciente.java
+++ src/gestorAplicacion/personas/Paciente.java
@@ -1,5 +1,44 @@
 package gestorAplicacion.personas;
+import gestorAplicacion.registrosMedicos.HistoriaClinica;
 
-public class Paciente {
+import java.util.ArrayList;
 
+
+
+public class Paciente extends Persona{
+	private String eps;
+	private ArrayList<String> sintomas;
+	private HistoriaClinica historiaClinica;
+	
+	public Paciente(String nombre, String apellido, tipoDocumento documento, long numeroDocumento, int edad, String genero, long telefono, String correoElectronico, String direccion, String eps, 
+			ArrayList<String> sintomas, HistoriaClinica historiaClinica) {
+		super(nombre, apellido, documento, numeroDocumento, edad, genero, telefono, correoElectronico, direccion);
+		setEps(eps);
+		setSintomas(sintomas);
+		setHistoriaClinica(historiaClinica);
+		
+	}
+	
+	public String getEps() {
+		return eps;
+	}
+	public void setEps(String eps) {
+		this.eps = eps;
+	}
+	
+	public ArrayList<String> getSintomas() {
+		return sintomas;
+	}
+	public void setSintomas(ArrayList<String> sintomas) {
+		this.sintomas = sintomas;
+	}
+	
+	public HistoriaClinica getHistoriaClinica() {
+		return historiaClinica;
+	}
+	public void setHistoriaClinica(HistoriaClinica historiaClinica) {
+		this.historiaClinica = historiaClinica;
+	}
+	
+	
 }
