a6c97b5dcec2299ccb05f678638071251dce99a5
Luzarait1 <111089098+Luzarait1@users.noreply.github.com>
2022-10-07 08:30:06 -0500

    Clase HistoriaClinica

--- src/gestorAplicacion/registrosMedicos/HistoriaClinica.java
+++ src/gestorAplicacion/registrosMedicos/HistoriaClinica.java
@@ -1,5 +1,34 @@
-package gestorAplicacion.registrosMedicos;
+package registrosMedicos;
+import java.util.ArrayList;
 
 public class HistoriaClinica {
+    private Paciente paciente;
+    private ArrayList<Diagnostico> diagnosticos  = new ArrayList<Diagnostico>();
+    
+    public HistoriaClinica(Paciente paciente, ArrayList<Diagnostico> diagnosticos) {
+        this.paciente = paciente;
+        this.diagnosticos = diagnosticos;
+    }
 
+    public Paciente getPaciente() {
+        return paciente;
+    }
+
+    public void setPaciente(Paciente paciente) {
+        this.paciente = paciente;
+    }
+
+    public ArrayList<Diagnostico> getDiagnosticos() {
+        return diagnosticos;
+    }
+
+    public void setDiagnosticos(ArrayList<Diagnostico> diagnosticos) {
+        this.diagnosticos = diagnosticos;
+    }
+
+    public String visualizarDiagnostico(Diagnostico diagnosticos, int id) {
+        return "El diagnostico es "+ diagnosticos;
+        return "El id del diagnostico es "+id;
+    }
+    
 }
