2681c8f5b2d2bb2ef9209bf1e894cba2ab6f784e
Yeferson05 <yaguilar@unal.edu.co>
2022-10-09 23:54:00 -0500

    medico

--- src/gestorAplicacion/personas/Medico.java
+++ src/gestorAplicacion/personas/Medico.java
@@ -1,5 +1,32 @@
 package gestorAplicacion.personas;
+import gestorAplicacion.Cita;
 
-public class Medico {
+public class Medico extends Persona{
+	private String especialidad;
+	private boolean disponibilidad;
+	
+    public Medico(String nombre, String apellido, tipoDocumento documento, long numeroDocumento, int edad,
+     String genero, long telefono, String correoElectronico, String direccion, String especialidad, boolean disponibilidad) {
+		super(nombre, apellido, documento, numeroDocumento, edad, genero, telefono, correoElectronico, direccion);
+		this.especialidad = especialidad;
+		this.disponibilidad = disponibilidad;
+		
+	}
+
+    public String getEspecialidad() {
+        return especialidad;
+    }
+
+    public void setEspecialidad(String especialidad) {
+        this.especialidad = especialidad;
+    }
+
+    public boolean isDisponibilidad() {
+        return disponibilidad;
+    }
+
+    public void setDisponibilidad(boolean disponibilidad) {
+        this.disponibilidad = disponibilidad;
+    }
 
 }
