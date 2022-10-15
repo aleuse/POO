478b753506fad171cae6ffa12c74ca3178de5cd3
Yeferson05 <yaguilar@unal.edu.co>
2022-10-15 14:39:45 -0500

    me

--- src/gestorAplicacion/personas/Medico.java
+++ src/gestorAplicacion/personas/Medico.java
@@ -10,7 +10,8 @@ import gestorAplicacion.Consultorio;
 import gestorAplicacion.tipoCita;
 
 public class Medico extends Persona{
-    private static ArrayList<Diagnostico> listado= new ArrayList<Diagnostico>(); 
+    private static ArrayList<Diagnostico> listado= new ArrayList<Diagnostico>();
+    tipoCita especialidad; 
 	private boolean disponibilidad;
 	private Map<LocalDateTime, Consulta> consultas = new TreeMap<LocalDateTime, Consulta>();
 
