f7af3c3ec13b902dd1aa987af6f692a81db764a4
Yeferson05 <yaguilar@unal.edu.co>
2022-10-14 19:02:53 -0500

    medi

--- src/gestorAplicacion/personas/Medico.java
+++ src/gestorAplicacion/personas/Medico.java
@@ -11,7 +11,7 @@ public class Medico extends Persona{
     private static ArrayList<Diagnostico> listado= new ArrayList<Diagnostico>();
 	private String especialidad;
 	private boolean disponibilidad;
-	//private Map<LocalDateTime, Consulta> consultas = new TreeMap<LocalDateTime, Consulta>();
+	private Map<LocalDateTime, Consulta> consultas = new TreeMap<LocalDateTime, Consulta>();
 	
     public Medico(String nombre, String apellido, tipoDocumento documento, long numeroDocumento, int edad,
      String genero, long telefono, String correoElectronico, String direccion, String especialidad, boolean disponibilidad) {
@@ -37,13 +37,21 @@ public class Medico extends Persona{
         this.disponibilidad = disponibilidad;
     }
 
-	//public Map<LocalDateTime, Consulta> getConsultas() {
-		//return consultas;
-	//}
+    public static ArrayList<Diagnostico> getListado() {
+        return listado;
+    }
 
-	//public void setConsultas(Map<LocalDateTime, Consulta> consultas) {
-	//	this.consultas = consultas;
-	//}
+    public static void setListado(ArrayList<Diagnostico> listado) {
+        Medico.listado = listado;
+    }
+
+    public Map<LocalDateTime, Consulta> getConsultas() {
+        return consultas;
+    }
+
+    public void setConsultas(Map<LocalDateTime, Consulta> consultas) {
+        this.consultas = consultas;
+    }
 
     public void crearDiagnostico(ArrayList<String> sintomas, String observacion, String medicamiento, int id){
         Diagnostico diag = new Diagnostico (sintomas, observacion, medicamiento, id);
