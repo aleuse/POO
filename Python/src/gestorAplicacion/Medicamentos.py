000fed1b3ae61d9c0d603a4fa2c4e00937f52aee
RogerVeraL <105682754+RogerVeraL@users.noreply.github.com>
2022-11-19 11:35:09 -0500

    Entrega, Medicamento, tipoMedicamento

---
+++ Python/src/gestorAplicacion/Medicamentos.py
@@ -0,0 +1,58 @@
+import tipoMedicamento
+class Medicamentos:
+    cantAcetaminofen =100
+    cantAmoxicilina =100
+    cantIbuprofeno =100
+    cantLagrimasArtificiales =20
+    _medicamiento = []
+
+    #constructor
+    def __init__(self, tipoMed, paciente, dosis, modoUso):
+        self._tipoMed = tipoMed
+        self._paciente = paciente
+        self._dosis = dosis #cantidad a consumir
+        self._modoUso = modoUso #consumir cada x horas
+        self._medicamiento.append(self)
+    
+    #getter & setter
+    def getTipoMed(self):
+        return self._tipoMed
+    def setTipoMed(self, tipoMed):
+        self._tipoMed = tipoMed
+
+    def getPaciente(self):
+        return self._paciente
+    def setPaciente(self, paciente):
+        self._paciente = paciente
+
+    def getDosis(self):
+        return self._dosis
+    def setDosis(self, dosis):
+        self._dosis = dosis
+
+    def getModoUso(self):
+        return self._modoUso
+    def setModoUso(self, modoUso):
+        self._modoUso = modoUso
+
+    def getMedicamiento(self):
+        return self._medicamiento
+
+    def setMedicamiento(self, medicamiento):
+        self._medicamiento = medicamiento
+
+    #metodos
+    #resta la cantidad de medicamento asignado del inventario de medicamentos
+    @staticmethod
+    def asignarMed(eleccion, cantidad):
+        if eleccion == tipoMedicamento.tipoMedicamento.ACETAMINOFEN.value[0]: #"ACETAMINOFEN"
+            Medicamentos.cantAcetaminofen=Medicamentos.cantAcetaminofen-cantidad
+        elif eleccion == tipoMedicamento.tipoMedicamento.AMOXICILINA.value[0]:
+            Medicamentos.cantAmoxicilina=Medicamentos.cantAmoxicilina-cantidad
+        elif eleccion == tipoMedicamento.tipoMedicamento.IBUPROFENO.value[0]:
+            Medicamentos.cantIbuprofeno=Medicamentos.cantIbuprofeno-cantidad
+        else:
+            Medicamentos.cantLagrimasArtificiales=Medicamentos.cantLagrimasArtificiales-cantidad
+    
+    
+
--- Python/src/gestorAplicacion/tipoMedicamento.py
+++ Python/src/gestorAplicacion/tipoMedicamento.py
@@ -1,5 +1,10 @@
 from enum import Enum
-
+class tipoMedicamento(Enum):
+    ACETAMINOFEN = ("ACETAMINOFEN", 140)
+    AMOXICILINA = ("AMOXICILINA", 250)
+    IBUPROFENO = ("IBUPROFENO", 190)
+    LAGRIMAS_ARTIFICIALES = ("LAGRIMAS_ARTIFICIALES", 8000)
+    
 """ 
 La clase TipoMedicamento de tipo Enum es utilizada para indicar el tipo de un medicamento y su costo asociado. Hay 4
 posibles objetos que podrían ser creados que son ACETAMINOFEN, AMOXICILINA, IBUPROFENO y LAGRIMAS_ARTIFICIALES con
@@ -7,25 +12,4 @@ su atributo de tipoMedicamento que indica su tipo y su atributo costo que indica
 unidad (en el caso de Lágrimas artificiales es por frasco).
 
 """
-class tipoMedicamento(Enum):
-    ACETAMINOFEN = ("ACETAMINOFEN", 140)
-    AMOXICILINA = ("AMOXICILINA", 250)
-    IBUPROFENO = ("IBUPROFENO", 190)
-    LAGRIMAS_ARTIFICIALES = ("LAGRIMAS_ARTIFICIALES", 8000)
     
-    def __init__(self, tipoMedicamento, costo):
-        self._tipoMedicamento = tipoMedicamento
-        self._costo = costo
-    
-    # Getters y Setters
-    def getTipoMedicamento(self):
-        return self._tipoMedicamento
-    
-    def setTipoMedicamento(self, tipoMedicamento):
-        self._tipoMedicamento = tipoMedicamento
-        
-    def getCosto(self):
-        return self._costo
-    
-    def setCosto(self, costo):
-        self._costo = costo
