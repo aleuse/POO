2cf7e37c3906a1d696d4c712f60866eea2f341aa
Luzarait <lcanasq@unal.edu.co>
2022-11-27 22:33:15 -0500

    Update pagoNomina.py

--- Python/src/pagoNomina.py
+++ Python/src/pagoNomina.py
@@ -32,16 +32,21 @@ class pagoNomina(Frame):
                          command=self.consultar)
         consultar.pack(padx=5, pady=5)
 
+        pagar = Button(master=botones, text="Pagar", font="Helvetica 11 bold", 
+                         bg="grey", fg="white", borderwidth=3, relief="raised",
+                         command=self.pagar)
+        pagar.pack(side=RIGHT, padx=5, pady=5)
+
         # Se crea el label que contendrá el mensaje con la cantidad de medicos con los que se cuenta.
         self.labelCantidad = Label(master=botones, font="Helvetica 10 bold", anchor=CENTER)
         botones.pack(padx=5, pady=5)
 
         # Se crea la tabla de los medicos.
         self.frameTabla = Frame(master=self)
-        self.tabla = ttk.Treeview(master=self.frameTabla, columns=(1, 2, 3, 4), show='headings')
+        self.tabla = ttk.Treeview(master=self.frameTabla, columns=(1, 2, 3, 4, 5), show='headings')
         self.tabla.pack(side=LEFT)
         # Se especifican los escabezados de la tabla.
-        encabezados = ("Nombre", "Apellido", "Documento", "Pagado")
+        encabezados = ("Nombre", "Apellido", "Documento", "Pagado", " ")
         
         # De acuerdo a los encabezados especificados, estos son ingresados como encabezados de la tabla.
         for i in range(len(encabezados)):
@@ -72,11 +77,20 @@ class pagoNomina(Frame):
         for med in Administrador.getMedicos():
             if med.isContratado() == True:
                 datos = (med.getNombre(), med.getApellido(), 
-                        str(med.getNumeroDocumento()), med.getNomina().get(datetime(2022, 10, 1, 00, 00)).isPagado())
+                        str(med.getNumeroDocumento()), med.getNomina().get(datetime(2022, 10, 1, 00, 00)).isPagado(), "Pagar")
                 self.tabla.insert(parent="", index="end", values=datos)
         # Por último se habilita para ser visualizada la tabla y el mensaje de la cantidad de medicos con los que se cuenta.
         self.labelCantidad.pack(padx=5, pady=5)
         self.frameTabla.pack(padx=5, pady=5)
+
+    def pagar():
+        for medico in Administrador.medicos:
+            if medico.isContratado() == True:
+                if medico.getNomina().get(datetime(2022, 10, 1, 00, 00)).isPagago() == False:
+                    medico.getNomina().get(datetime(2022, 10, 1, 00, 00)).isPagago() == True
+                    messagebox.showinfo("Aviso", "Se ha realizado el pago de nomina exitosamente")
+                else:
+                    messagebox.showinfo("Aviso", "Ya se le ha realizado el pago de nomina a este medico")                    
 
     # Por medio del método ocultarTabla() se deshabilita la visualización de las tablas cuando se cambia de menú.
     def ocultarTabla(self):
